399e8a149215d4cf6c62f4c2a4c3598a079cccc6
Martin <marthen7@gmail.com>
2019-06-07 22:12:39 +0800

    create function

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,10 +1,18 @@
 class MetricsController < ApplicationController
-	def index
-	end
+  def index
+  end
 
-	def action_plan
-	end
+  def action_plan
+  end
 
-	def create_action_plan
+  def create_action_plan
+    ActionPlan.create(action_plan_params)
+  end
+
+  private
+
+  def action_plan_params
+    params.require(:create_plan).permit(:qcm, :date, :agent, :specific_behavior, :measurable,
+                                        :action, :relevant, :timebound, :why)
   end
 end
