b28b2a682734a106e0463b39ea89e3f8c9b1e5af
Martin <marthen7@gmail.com>
2019-06-07 13:54:48 +0800

    add action plan routes

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -1,4 +1,7 @@
 class MetricsController < ApplicationController
 	def index
 	end
+
+	def action_plan
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "metrics#index"
+
+  resources :metrics do
+  	collection do 
+  	  get 'action_plan'
+  	end
+  end
 end
