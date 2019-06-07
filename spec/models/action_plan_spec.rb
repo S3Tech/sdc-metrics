b598619f09c27df2d0cce7f3438db092a170868d
Martin <marthen7@gmail.com>
2019-06-07 15:52:16 +0800

    set spec

--- spec/models/action_plan_spec.rb
+++ spec/models/action_plan_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe ActionPlan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "is valid with valid attributes" do
+    expect(ActionPlan.new).to be_valid
+  end
 end
