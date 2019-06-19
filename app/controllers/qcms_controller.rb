2315f581f57fa094ed12614ebd9b13330cd5b297
Martin <marthen7@gmail.com>
2019-06-19 14:37:18 +0800

    qcm create function

--- app/controllers/qcms_controller.rb
+++ app/controllers/qcms_controller.rb
@@ -1,4 +1,24 @@
 class QcmsController < ApplicationController
-	def index
-	end
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    qcm = Qcm.new(qcm_params)
+    if qcm.save
+      flash[:notice] = "Action Plan successfully created"
+      redirect_to qcms_path
+    else
+      flash[:success] = "Error"
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def qcm_params
+    params.require(:qcm).permit(:first_name, :last_name, :status)
+  end
 end
