d30e055ffba90027c5d7d62d25019442254f0e25
Martin <marthen7@gmail.com>
2019-06-19 14:11:31 +0800

    create form for agents

--- app/controllers/agents_controller.rb
+++ app/controllers/agents_controller.rb
@@ -1,2 +1,25 @@
 class AgentsController < ApplicationController
+  def index
+  end
+
+  def new
+    @agent = Agent.new
+  end
+
+  def create
+    agent = Agent.new(agent_params)
+    if agent.save
+      flash[:notice] = "Action Plan successfully created"
+      redirect_to agents_path
+    else
+      flash[:error] = "Error"
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def agent_params
+    params.require(:agent).permit(:first_name, :last_name, :status)
+  end
 end
